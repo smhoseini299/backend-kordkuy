73ec864a1cdfe666035d46fd069b1b9adeec11c4
m.hoseini <smhoseini299@gmail.com>
2025-09-22 23:10:39 +0330

    cors api

--- bootstrap/cache/config.php
+++ bootstrap/cache/config.php
@@ -204,6 +204,34 @@
     ),
     'prefix' => 'laravel_cache_',
   ),
+  'cors' => 
+  array (
+    'paths' => 
+    array (
+      0 => 'api/*',
+      1 => 'sanctum/csrf-cookie',
+    ),
+    'allowed_methods' => 
+    array (
+      0 => '*',
+    ),
+    'allowed_origins' => 
+    array (
+      0 => '*',
+    ),
+    'allowed_origins_patterns' => 
+    array (
+    ),
+    'allowed_headers' => 
+    array (
+      0 => '*',
+    ),
+    'exposed_headers' => 
+    array (
+    ),
+    'max_age' => 0,
+    'supports_credentials' => false,
+  ),
   'database' => 
   array (
     'default' => 'mysql',
